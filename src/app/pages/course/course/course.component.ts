02d1981b659baf3b7b1f3e4f4d8780f9a9ba58a8
mehdawi adham <mehdawiadham@gmail.com>
2024-09-23 13:44:12 +0300

    adding matirials

--- src/app/pages/course/course/course.component.ts
+++ src/app/pages/course/course/course.component.ts
@@ -3,18 +3,20 @@ import { CourseModel } from '../../../Models/courseModel';
 import { CourseDataService } from '../../../services/course-data.service';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import {MatTableModule} from '@angular/material/table';
+
 
 @Component({
   selector: 'app-course',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,MatTableModule],
   templateUrl: './course.component.html',
   styleUrls: ['./course.component.css'],
 })
 export class CourseComponent implements OnInit {
   title: string = 'Course List';
   @Input() courses: CourseModel[] = [];
-
+  displayedColumns:string[]= ['id','name','description']
   constructor(
     public courseDataService: CourseDataService,
     private router: Router
