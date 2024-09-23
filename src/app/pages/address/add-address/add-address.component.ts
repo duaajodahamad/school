a71a02ea81a34ac56d5eaacc82dce7564216fae1
tawfiqin <wael255@live.com>
2024-09-23 11:41:07 +0300

    Forcefully merged Feature/Address into master, taking all changes from Feature/Address

--- src/app/pages/address/add-address/add-address.component.ts
+++ src/app/pages/address/add-address/add-address.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+  FormArray,
+} from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { AddressModel } from '../../../Models/addressModel';
 import { AddressDataService } from '../../../services/address-data.service';
@@ -15,7 +21,7 @@ import { Subscription } from 'rxjs';
 })
 export class AddAddressComponent implements OnInit, OnDestroy {
   studentId!: number;
-  addressForm!: FormGroup;
+  form!: FormGroup;
   addresses: AddressModel[] = [];
   subscription!: Subscription;
 
@@ -32,7 +38,17 @@ export class AddAddressComponent implements OnInit, OnDestroy {
       this.loadAddresses();
     });
 
-    this.addressForm = this.fb.group({
+    this.form = this.fb.group({
+      addressForms: this.fb.array([this.createAddressForm()]),
+    });
+  }
+
+  get addressForms(): FormArray {
+    return this.form.get('addressForms') as FormArray;
+  }
+
+  createAddressForm(): FormGroup {
+    return this.fb.group({
       street: ['', Validators.required],
       city: ['', Validators.required],
       neighborhood: [''],
@@ -44,24 +60,40 @@ export class AddAddressComponent implements OnInit, OnDestroy {
     });
   }
 
+  addAddressForm() {
+    this.addressForms.push(this.createAddressForm());
+  }
+
+  removeAddressForm(index: number) {
+    if (this.addressForms.length > 1) {
+      this.addressForms.removeAt(index);
+    }
+  }
+
   loadAddresses() {
     this.subscription = this.addressService.addresses.subscribe((addressList) => {
-      this.addresses = addressList.filter((address) => address.studentId === this.studentId);
+      this.addresses = addressList.filter(
+        (address) => address.studentId === this.studentId
+      );
     });
   }
 
-  addAddress() {
-    const newAddress: AddressModel = {
-      id: this.addressService.getNextId(),
-      studentId: this.studentId,
-      ...this.addressForm.value,
-    };
-
-    this.addressService.addAddress(newAddress);
-
-    this.addressForm.reset();
+  addAddresses() {
+    for (let i = 0; i < this.addressForms.length; i++) {
+      const addressFormGroup = this.addressForms.at(i) as FormGroup;
+      if (addressFormGroup.valid) {
+        const newAddress: AddressModel = {
+          id: this.addressService.getNextId(),
+          studentId: this.studentId,
+          ...addressFormGroup.value,
+        };
+        this.addressService.addAddress(newAddress);
+      }
+    }
+    // Reset the FormArray
+    this.addressForms.clear();
+    this.addressForms.push(this.createAddressForm());
   }
-
 
   navigateTo(route: string, id?: number) {
     if (id !== undefined) {
@@ -70,7 +102,6 @@ export class AddAddressComponent implements OnInit, OnDestroy {
       this.router.navigate([`/${route}`]);
     }
   }
-
 
   ngOnDestroy() {
     if (this.subscription) {
