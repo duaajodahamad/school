e3777d2bb6bd1921041420a09663176e32715903
wael salameh <90796176+superwae@users.noreply.github.com>
2024-09-24 09:11:35 +0300

    StudentListAngularMatrial

--- src/app/pages/semester/semester-list/semester-list.component.ts
+++ src/app/pages/semester/semester-list/semester-list.component.ts
@@ -1,17 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { SemesterDataService, SemesterModel } from '../../../services/semester-data.service';
 import { CommonModule } from '@angular/common';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSort } from '@angular/material/sort';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-semester-list',
   templateUrl: './semester-list.component.html',
   styleUrls: ['./semester-list.component.css'],
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatButtonModule,
+    MatIconModule
+  ],
 })
 export class SemesterListComponent implements OnInit {
   semesters: SemesterModel[] = [];
+  displayedColumns: string[] = ['id', 'name', 'startDate', 'endDate', 'action'];
+  dataSource = new MatTableDataSource<SemesterModel>(this.semesters);
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  
   title: string = 'Semester List';
   constructor(private router: Router, private semesterDataService: SemesterDataService) {}
 
@@ -27,6 +43,11 @@ export class SemesterListComponent implements OnInit {
     } else {
       this.router.navigate([`/${route}`]);
     }
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
 
   deleteSemester(semester: SemesterModel): void {
--- src/app/pages/student/student-list/student-list.component.ts
+++ src/app/pages/student/student-list/student-list.component.ts
@@ -1,33 +1,63 @@
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
-import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 import { StudnetModel } from '../../../Models/student.modle';
 import { StudentdataService } from '../../../services/studentdata.service';
 import { Router } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSortModule } from '@angular/material/sort';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-student-list',
-  standalone: true,
-  imports: [CommonModule,FormsModule],
   templateUrl: './student-list.component.html',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatTableModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatSortModule,
+    MatPaginatorModule,
+  ],
   styleUrls: ['./student-list.component.css'],
 })
-export class StudentListComponent implements OnInit,OnChanges,OnDestroy {
+export class StudentListComponent implements OnInit, OnChanges, OnDestroy {
   title: string = 'Student List';
-  sudentSoucrReference:any;
+  sudentSoucrReference: any;
   @Input() students: StudnetModel[] = [];
-  test!:string;
+  test!: string;
+  displayedColumns: string[] = ['stId', 'Name', 'email', 'phone', 'actions'];
+  dataSource = new MatTableDataSource<StudnetModel>(this.students);
+
   constructor(
     public studentservice: StudentdataService,
     private router: Router
   ) {}
+
   ngOnDestroy(): void {
     this.sudentSoucrReference.unsubscribe();
   }
 
   ngOnInit() {
-    this.sudentSoucrReference= this.studentservice.students.subscribe((c) => {
+    this.sudentSoucrReference = this.studentservice.students.subscribe((c) => {
       this.students = c;
+      this.dataSource.data = c;
     });
   }
 
@@ -43,12 +73,9 @@ export class StudentListComponent implements OnInit,OnChanges,OnDestroy {
     this.studentservice.deleteStudent(id);
   }
 
-  ngOnChanges(changes: SimpleChanges): void {
-    alert("hh");
-  }
+  ngOnChanges(changes: SimpleChanges): void {}
 
-
-  search(){
-      this.studentservice.search(this.test);
+  search() {
+    this.studentservice.search(this.test);
   }
 }
